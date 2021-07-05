ea60d3f9b194a1be56e61c7ee06d9ba167bb7f97
YouijngYu99 <yuyoujing@hotmail.com>
2021-07-05 20:10:17 +0100

    Fixed some version bugs. Can't download spacy en module.

--- utils.py
+++ utils.py
@@ -7,12 +7,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 import heapq  
 import spacy 
-nlp = spacy.load('en')
+nlp = spacy.load("en_core_web_sm")
 # Pkgs for Normalizing Text
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 # Import Heapq for Finding the Top N Sentences
 from heapq import nlargest
+from gensim.summarization.summarizer import summarize
 import gensim.summarization 
 
 
@@ -88,4 +89,4 @@ def text_summarizer(raw_docx):
     return summary
 
 def summarize(raw_text):
-    return gensim.summarization.summarize(raw_text)
+    return summarize(raw_text)
