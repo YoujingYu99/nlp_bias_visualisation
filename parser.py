902f96b86189f30aec8a8ac5804c3d9ef34d9b8e
X Hu <xh297@cam.ac.uk>
2021-07-05 17:23:26 +0100

    start with the framework

---
+++ parser.py
@@ -0,0 +1,5 @@
+"""
+This file is to translate the natural language input at the web interface 
+to query language that can be used to extract information from the corpora
+"""
+
---
+++ utils.py
@@ -0,0 +1,91 @@
+"""
+This file contains the basic functions to extract information from the text corpus
+"""
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import heapq  
+import spacy 
+nlp = spacy.load('en')
+# Pkgs for Normalizing Text
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+# Import Heapq for Finding the Top N Sentences
+from heapq import nlargest
+import gensim.summarization 
+
+
+def nltk_summarizer(raw_text):
+	stopWords = set(stopwords.words("english"))
+	word_frequencies = {}  
+	for word in nltk.word_tokenize(raw_text):  
+	    if word not in stopWords:
+	        if word not in word_frequencies.keys():
+	            word_frequencies[word] = 1
+	        else:
+	            word_frequencies[word] += 1
+
+	maximum_frequncy = max(word_frequencies.values())
+
+	for word in word_frequencies.keys():  
+	    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+	sentence_list = nltk.sent_tokenize(raw_text)
+	sentence_scores = {}  
+	for sent in sentence_list:  
+	    for word in nltk.word_tokenize(sent.lower()):
+	        if word in word_frequencies.keys():
+	            if len(sent.split(' ')) < 30:
+	                if sent not in sentence_scores.keys():
+	                    sentence_scores[sent] = word_frequencies[word]
+	                else:
+	                    sentence_scores[sent] += word_frequencies[word]
+
+
+
+	summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+	summary = ' '.join(summary_sentences)  
+	return summary
+
+def text_summarizer(raw_docx):
+    raw_text = raw_docx
+    docx = nlp(raw_text)
+    stopwords = list(STOP_WORDS)
+    # Build Word Frequency # word.text is tokenization in spacy
+    word_frequencies = {}  
+    for word in docx:  
+        if word.text not in stopwords:
+            if word.text not in word_frequencies.keys():
+                word_frequencies[word.text] = 1
+            else:
+                word_frequencies[word.text] += 1
+
+
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():  
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+    # Sentence Tokens
+    sentence_list = [ sentence for sentence in docx.sents ]
+
+    # Sentence Scores
+    sentence_scores = {}  
+    for sent in sentence_list:  
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if len(sent.text.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+
+
+    summarized_sentences = nlargest(7, sentence_scores, key=sentence_scores.get)
+    final_sentences = [ w.text for w in summarized_sentences ]
+    summary = ' '.join(final_sentences)
+    return summary
+
+def summarize(raw_text):
+    return gensim.summarization.summarize(raw_text)
