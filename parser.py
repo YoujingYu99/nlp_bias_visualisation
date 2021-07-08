bcc2a130a0c9aa32fbde2fb80ea5a31666ccb87e
X Hu <xh297@cam.ac.uk>
2021-07-08 11:12:41 +0100

    adding in the reader to read from the files forming objects except rst

--- parser.py
+++ parser.py
@@ -2,4 +2,12 @@
 This file is to translate the natural language input at the web interface 
 to query language that can be used to extract information from the corpora
 """
+import os
+
+nl_query = "Are most nurses women ?" # pre-set, should be attained from the webpage input
+database = "amalgum" # should be attained from the webpage input
+
+"""need to be redefined in later stages"""
+path = os.path.join(database,database,'data','wikinews','AA')
+
 
