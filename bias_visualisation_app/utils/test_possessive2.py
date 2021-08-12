0ebcd812fcdfd1323792d6b8c56f72f5eb2be2fc
YoujingYu99 <yuyoujing@hotmail.com>
2021-08-12 13:49:54 +0100

    Finished writing the possessive and possessor functions

--- bias_visualisation_app/utils/test_possessive2.py
+++ bias_visualisation_app/utils/test_possessive2.py
@@ -15,7 +15,7 @@ female_nouns = nc.names.words('female.txt')
 female_nouns.extend(['she', 'her', 'herself', 'lady', 'ladys', 'woman', 'women', 'female'])
 female_nouns = [x.lower() for x in female_nouns]
 
-sentence = "We need to protect women's rights. Men's health is as important. I can look after the Simpsons' cat."
+sentence = "We need to protect women's rights. Men's health is as important. I can look after the Simpsons' cat. Japan's women live longest. Canada's John clinged a gold prize."
 
 
 def findfemale_possessives(sent):
@@ -39,6 +39,26 @@ def findfemale_possessives(sent):
 
     return female_possessive_list
 
+def findfemale_possessors(sent):
+    # e.g. Norway's women
+    tokens = nltk.word_tokenize(sent)
+    tags = nltk.pos_tag(tokens)
+    female_possessor_list = []
+    for female_noun in female_nouns:
+        try:
+            # You are interested in splitting the sentence here
+            sentence_split = tokens.index(female_noun)
+            # Find the words where tag meets your criteria (must be a noun / proper noun)
+            if tokens[sentence_split - 1] == "'s":
+                possessor = tokens[sentence_split - 2]
+                if tags[sentence_split - 2][1].startswith('NN'):
+                    female_possessor_list.append(possessor)
+            else:
+                pass
+        except:
+            continue
+
+    return female_possessor_list
 
 def findmale_possessives(sent):
     # e.g. men's shoes
@@ -61,21 +81,47 @@ def findmale_possessives(sent):
 
     return male_possessive_list
 
+def findmale_possessors(sent):
+    # e.g. Norway's women
+    tokens = nltk.word_tokenize(sent)
+    tags = nltk.pos_tag(tokens)
+    male_possessor_list = []
+    for male_noun in male_nouns:
+        try:
+            # You are interested in splitting the sentence here
+            sentence_split = tokens.index(male_noun)
+            # Find the words where tag meets your criteria (must be a noun / proper noun)
+            if tokens[sentence_split - 1] == "'s":
+                possessor = tokens[sentence_split - 2]
+                if tags[sentence_split - 2][1].startswith('NN'):
+                    male_possessor_list.append(possessor)
+            else:
+                pass
+        except:
+            continue
+
+    return male_possessor_list
 
 def determine_gender_possessive_test(sentence):
     sentence = sentence.lower()
     sent_text = nltk.sent_tokenize(sentence)
     tot_female_possessive_list = []
     tot_male_possessive_list = []
+    tot_female_possessor_list = []
+    tot_male_possessor_list = []
     for sent in sent_text:
         try:
             female_possessive_list = findfemale_possessives(sent)
             male_possessive_list = findmale_possessives(sent)
+            female_possessor_list = findfemale_possessors(sent)
+            male_possessor_list = findmale_possessors(sent)
             tot_female_possessive_list.extend(female_possessive_list)
             tot_male_possessive_list.extend(male_possessive_list)
+            tot_female_possessor_list.extend(female_possessor_list)
+            tot_male_possessor_list.extend(male_possessor_list)
         except:
             continue
-    return tot_female_possessive_list, tot_male_possessive_list
+    return tot_female_possessive_list, tot_male_possessive_list, tot_female_possessor_list, tot_male_possessor_list
 
 
 print(determine_gender_possessive_test(sentence))
