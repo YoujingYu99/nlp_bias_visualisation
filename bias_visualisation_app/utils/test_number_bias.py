3df83ce24a06386ee36c5a602b06a4fc0619720c
YoujingYu99 <yuyoujing@hotmail.com>
2021-08-19 17:38:30 +0100

    Successfully testing debiasing input data.

--- bias_visualisation_app/utils/test_number_bias.py
+++ bias_visualisation_app/utils/test_number_bias.py
@@ -33,6 +33,7 @@ def txt_list():
 
 def calculate_sentence_bias_score(word_list):
     view_df = load_total_dataframe(name='total_dataframe_user_uploads')
+    sentence_score_list = []
     for sent in word_list:
         bias_list = []
         for word in sent:
@@ -41,13 +42,35 @@ def calculate_sentence_bias_score(word_list):
                 bias_list.append(bias_value)
             except:
                 continue
+
         if len(bias_list) == 0:
             mean_bias_score = 0
         else:
             mean_bias_score = sum(bias_list)/len(bias_list)
-        print(mean_bias_score)
+
+        sentence_score = {'sentence': sent, 'score': mean_bias_score}
+        sentence_score_list.append(sentence_score)
+        sentence_score_df = pd.DataFrame(sentence_score_list)
+
+    return sentence_score_df
 
 word_list = txt_list()
+sentence_score_df = calculate_sentence_bias_score(word_list)
 
-print(calculate_sentence_bias_score(word_list))
+def debiased_file(threshold_value, sentence_score_df):
+    debiased_df = sentence_score_df.loc[sentence_score_df['score'] > threshold_value]
+    debiased_sentence_list = []
+    for index, row in debiased_df.iterrows():
+        sentence = row['sentence']
+        new_sentence = ' '.join(str(x) for x in sentence) + '.'
+        debiased_sentence_list.append(new_sentence)
+
+    path_parent = os.path.dirname(os.getcwd())
+    save_path = os.path.join(path_parent, 'static')
+
+    with open(os.path.join(save_path, 'debiased_file'), 'w+', encoding='utf-8') as f:
+        f.write('\n'.join(debiased_sentence_list))
+
+
+debiased_file(threshold_value=0.5, sentence_score_df=sentence_score_df)
 
