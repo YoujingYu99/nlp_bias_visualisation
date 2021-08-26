a8de5ed1e4370c5aae91bc2386776efc6b9973bc
YoujingYu99 <yuyoujing@hotmail.com>
2021-08-26 10:18:17 +0100

    optimising the functions further; regrouping elements in parameters.

--- delete.py
+++ delete.py
@@ -5,17 +5,17 @@ fileDir = os.path.dirname(os.path.realpath('__file__'))
 txt_dir = os.path.join(fileDir, 'bias_visualisation_app')
 
 
-def delete_all_in_dir(folder_path):
-
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+def delete_all_in_dir(folder_path_list):
+    for folder_path in folder_path_list:
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def delete_all_with_ext(folder_path, ext_list):
     files_in_directory = os.listdir(folder_path)
@@ -28,10 +28,6 @@ def delete_all_with_ext(folder_path, ext_list):
 
 
 
-delete_all_in_dir(os.path.join(txt_dir, 'data', 'user_uploads'))
-
-delete_all_in_dir(os.path.join(txt_dir, 'static', 'user_uploads'))
-
-delete_all_in_dir(os.path.join(txt_dir, 'static', 'user_downloads'))
+delete_all_in_dir(folder_path_list=[os.path.join(txt_dir, 'static', 'user_downloads'), os.path.join(txt_dir, 'static', 'user_uploads'), os.path.join(txt_dir, 'static', 'user_downloads')])
 
 delete_all_with_ext(folder_path=os.path.join(txt_dir, 'static'), ext_list=['.html', '.png', '.txt', '.csv'])
