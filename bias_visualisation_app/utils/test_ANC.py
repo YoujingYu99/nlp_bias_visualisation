e90ed6f14098177dd26c3f7339467ed78cb275c6
YoujingYu99 <yuyoujing@hotmail.com>
2021-09-07 11:45:17 +0100

    Testing file limit to be 500 KB. convert xml to txt

--- bias_visualisation_app/utils/test_ANC.py
+++ bias_visualisation_app/utils/test_ANC.py
@@ -24,25 +24,27 @@ def txt_list(txt_dir):
     :param txt_dir: the path of the txt files to be extracted
     :return: a clean list containing the raw sentences
     """
+    count = 0
     training_list = []
     txt_files = os.listdir(txt_dir)
     file_n = len(txt_files)
     print('{} files being processed'.format(file_n))
     for file in txt_files:
-        if file.endswith(".txt"):
-            with open(os.path.join(txt_dir, file), 'r', encoding='utf-8') as file_in:
-                for line in file_in:
-                    # create word tokens as well as remove puntuation in one go
-                    rem_tok_punc = RegexpTokenizer(r'\w+')
-                    tokens = nltk.word_tokenize(line)
-                    # convert the words to lower case
-                    words = [w.lower() for w in tokens]
-                    # Remove stop words
-                    words = [w for w in words ]
-                    words = ' '.join(words)
-                    training_list.append(words)
-
-    with open(os.path.join(txt_dir, "face-to-face_total.txt"), 'w', encoding='utf-8') as output:
+        while count < 45:
+            if file.endswith(".txt"):
+                with open(os.path.join(txt_dir, file), 'r', encoding='utf-8') as file_in:
+                    for line in file_in:
+                        # create word tokens as well as remove puntuation in one go
+                        rem_tok_punc = RegexpTokenizer(r'\w+')
+                        tokens = nltk.word_tokenize(line)
+                        # convert the words to lower case
+                        words = [w.lower() for w in tokens]
+                        # Remove stop words
+                        words = [w for w in words ]
+                        words = ' '.join(words)
+                        training_list.append(words)
+                count += 1
+    with open(os.path.join(txt_dir, "written2_test.txt"), 'w', encoding='utf-8') as output:
         for row in training_list:
             output.write(str(row) + '\n')
 
@@ -50,6 +52,6 @@ def txt_list(txt_dir):
     return training_list
 
 path_parent = os.path.dirname(os.getcwd())
-test_path = os.path.join(path_parent, 'data', 'ANC_face-to-face')
+test_path = os.path.join(path_parent, 'data', 'ANC_written2')
 
 txt_list(test_path)
--- bias_visualisation_app/utils/test_xml.py
+++ bias_visualisation_app/utils/test_xml.py
@@ -1,45 +1,44 @@
 import os
 import xml.etree.ElementTree as ET
 
-sample_list = []
 
-def save_xml_text(user_text):
+def save_xml_text(file_name, user_text):
     # user inputs a string
     fileDir = os.path.dirname(os.path.realpath('__file__'))
 
     # os.path.join is used so that paths work in every operating system
-    save_user_path = os.path.join(fileDir, '..', 'data', 'enwiki_xml')
-
-    with open(os.path.join(save_user_path, 'sample_xml.txt'), 'w+', encoding='utf-8') as f:
+    save_user_path = os.path.join(fileDir, '..', 'data', 'enwiki_txt')
+    with open(os.path.join(save_user_path, str(file_name) + '.txt'), 'w+', encoding='utf-8') as f:
        f.write(user_text)
 
-def etree_reader(path, file):
+def etree_reader(path):
     """
     :param path: the path into amalgum dataset
     :param file: the file name in the folder: amalgum_genre_docxxx
     :return: an element tree object
     """
-    file += '.xml'
-    if os.path.exists(os.path.join(path, file)):
-        tree = ET.parse(os.path.join(path, file))
-        root = tree.getroot()
-        for child in root:
-            for child_a in child:
-                # for child_b in child_a:
-                #     print(child_b.text)
-                for i in child_a.itertext():
-                    sample_list.append(i)
+    xml_files = os.listdir(path)
+    file_n = len(xml_files)
+    print('{} files being processed'.format(file_n))
+    count = 0
+    for file in xml_files:
+        if file.endswith(".xml"):
+            sample_list = []
+            tree = ET.parse(os.path.join(path, file))
+            root = tree.getroot()
+            for child in root:
+                for child_a in child:
+                    for i in child_a.itertext():
+                        sample_list.append(i)
+            count += 1
 
-        new_list = ' '.join(sample_list)
-        new_string = "".join(str(x) for x in new_list)
-        save_xml_text(new_string)
-    else:
-        print(os.path.join(path, file))
-        print('file not found')
-        pass
+            new_list = ' '.join(sample_list)
+            new_string = "".join(str(x) for x in new_list)
+            print(new_string)
+            save_xml_text(file_name=count, user_text=new_string)
 
 path_parent = os.path.dirname(os.getcwd())
-test_path = os.path.join(path_parent, 'data')
+test_path = os.path.join(path_parent, 'data', 'enwiki_xml')
 
 
-etree_reader(path=test_path, file="sample-monolingual")
+etree_reader(path=test_path)
