b19382d513c3f48a0d432f46a9cb7dc72e4cb529
YoujingYu99 <yuyoujing@hotmail.com>
2021-09-22 12:16:58 +0100

    adding nltk.names in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # import numpy.random.common
 # import numpy.random.bounded_integers
 # import numpy.random.entropy
+import nltk
+nltk.download('names')
 from flask_cors import CORS
 from bias_visualisation_app import app
 
