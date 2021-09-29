cc58dd6a01bf9ea357def1feb5695d29ab3787e7
YoujingYu99 <yuyoujing@hotmail.com>
2021-09-29 13:02:12 +0100

    rewriting delete functions and updating .fitkeep files.

--- delete.py
+++ delete.py
@@ -17,20 +17,19 @@ def delete_all_in_dir(folder_path_list):
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-def delete_all_with_ext(folder_path, ext_list):
-    files_in_directory = os.listdir(folder_path)
-    for ext in ext_list:
-        filtered_files = [file for file in files_in_directory if file.endswith(ext) and os.path.splitext(os.path.basename(file))[0] != 'nothing_here']
-        for file in filtered_files:
-            path_to_file = os.path.join(folder_path, file)
-            os.remove(path_to_file)
+def delete_all_with_ext(folder_path_list, ext_list):
+    for folder_path in folder_path_list:
+        files_in_directory = os.listdir(folder_path)
+        for ext in ext_list:
+            filtered_files = [file for file in files_in_directory if file.endswith(ext) and os.path.splitext(os.path.basename(file))[0] != 'nothing_here']
+            for file in filtered_files:
+                path_to_file = os.path.join(folder_path, file)
+                os.remove(path_to_file)
 
 
 
 
-delete_all_in_dir(folder_path_list=[os.path.join(app_dir, 'static', 'user_uploads_text'), os.path.join(app_dir, 'static', 'user_uploads'), os.path.join(app_dir, 'static', 'user_downloads')])
+
+delete_all_with_ext(folder_path_list=[os.path.join(app_dir, 'static', 'user_uploads_text'), os.path.join(app_dir, 'static', 'user_uploads'), os.path.join(app_dir, 'static', 'user_downloads')], ext_list=['.html', '.png', '.txt', '.csv', '.xlsx'])
 
 
-delete_all_with_ext(folder_path=os.path.join(app_dir, 'static'), ext_list=['.html', '.png', '.txt', '.csv', '.gif'])
-
-delete_all_with_ext(folder_path=os.path.join(app_dir, 'utils', 'tests'), ext_list=['.csv', '.xlsx', '.txt'])
